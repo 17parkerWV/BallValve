177c07b8b8bfcdc18f3539822c2cf5ebadd742ae
David Parker <dparke29@mywvm.wvm.edu>
2020-09-16 12:12:39 -0700

    This is set up so a button controls the fet with pin 12 and there is a pulldown resistor for when the button is released

--- BallValve/BallValve.ino
+++ BallValve/BallValve.ino
@@ -1,9 +1,44 @@
 //This program is made to control a ball valve using the input from a flow rate sensor to maintain correct... waterness?
-void setup() {
+//Interrupt pins for the Arduino Nano are on digital pins 2&3
 
+//These are for counting the spins
+volatile unsigned int spinCount = 0;
+const int spinTime = 300;
+
+//These are the pins I am using
+const int sensorPin = 2;
+const int gatePin = 13;
+const int outPin = 12;
+
+//Function Prototypes
+void goCount();
+int resetCount();
+
+
+void setup() {
+	Serial.begin(115200);
+	attachInterrupt(digitalPinToInterrupt(sensorPin), goCount, FALLING);
+	//pinMode(gatePin, OUTPUT);
+	pinMode(outPin, OUTPUT);
+	digitalWrite(outPin, HIGH);
+	//digitalWrite(gatePin, LOW);
 }
 
 
 void loop() {
-  
+	int currentMillis = millis();
+	while (millis() - currentMillis < spinTime) {}
+	detachInterrupt(sensorPin);
+	resetCount();
+	attachInterrupt(sensorPin, goCount, FALLING);
+}
+
+
+void goCount() {
+	spinCount++;
+}
+
+int resetCount() {
+	Serial.println(spinCount);
+	spinCount = 0;
 }
