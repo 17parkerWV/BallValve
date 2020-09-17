18fdd8384741cb2fa9c719cb3ec21b1f9de6460e
David Parker <dparke29@mywvm.wvm.edu>
2020-09-16 19:06:13 -0700

    I got 2 projection points and it's not super accurate, it was better but I messed it up, I have to work on the conditions and stuff on line 50.

--- BallValve/BallValve.ino
+++ BallValve/BallValve.ino
@@ -3,7 +3,9 @@
 
 //These are for counting the spins
 volatile unsigned long spinCount = 0;
-const int spinTime = 300;
+const int spinTime = 100;
+//maximum 90 for 300ms counting time, scales
+const int target = 30;
 
 //These are the pins I am using
 const int sensorPin = 2;
@@ -12,42 +14,51 @@ const int gatePin = 13;
 //Function Prototypes
 void updatePower();
 void addCount();
-void resetCount();
 
+//for the battery level array
+int yn, yn1, yn2, yn3;
 
 void setup() {
 	attachInterrupt(digitalPinToInterrupt(sensorPin), addCount, FALLING);
 	pinMode(gatePin, OUTPUT);
 	pinMode(sensorPin, INPUT_PULLUP);
 	digitalWrite(gatePin, LOW);
+	Serial.begin(115200);
 }
 
 
 void loop() {
 	delay(spinTime);
 	updatePower();
-	resetCount();
 }
+
 
 void updatePower() {
 	cli();
-	if (spinCount <= 40) {
+
+	yn3 = yn2;
+	yn2 = yn1;
+	yn1 = yn;
+	yn = spinCount;
+	int projPoint = (yn + (2 * yn - 4 * yn1 + 3 * yn2 - yn3) / (spinTime/100));
+	int projPoint2 = (projPoint + (2 * projPoint - 4 * yn + 3 * yn1 - yn2) / (spinTime/100));
+	Serial.print(spinCount);
+	Serial.print('\t');
+	Serial.print(projPoint);
+	Serial.print('\t');
+	Serial.println(projPoint2);
+	if ((yn<20 || projPoint<=19)) {
 		digitalWrite(gatePin, HIGH);
+		Serial.println(F("POWER ON"));
+		Serial.println("");
 	}
-	else if (spinCount >= 50) {
+	else {
 		digitalWrite(gatePin, LOW);
 	}
+	spinCount = 0;
 	sei();
 }
-
-
 
 void addCount() {
 	spinCount++;
-}
-
-void resetCount() {
-	cli();
-	spinCount = 0;
-	sei();
 }
