b7f71a20863e867185dae251d0c99af962a48295
David Parker <dparke29@mywvm.wvm.edu>
2020-10-07 20:10:08 -0700

    Testing, valve is full open and it is just to get it to print the count

--- BallValve/BallValve.ino
+++ BallValve/BallValve.ino
@@ -1,9 +1,48 @@
 //This program is made to control a ball valve using the input from a flow rate sensor to maintain correct... waterness?
-void setup() {
 
+//Gate pin on the FET that controls the ball valve
+const int valveGatePin = 13;
+
+//These are for the Counter IC
+const int dataPins[10] = { 2,3,4,5,6,7,8,9,10,11 };
+const int counterResetPin = 12;
+unsigned int counterData;
+void getCount();
+
+
+
+void setup() {
+	for (int pinLoop = 0; pinLoop <= 9; pinLoop++) {
+		pinMode(dataPins[pinLoop], OUTPUT);
+	}
+	pinMode(valveGatePin, OUTPUT);
+	pinMode(counterResetPin, OUTPUT);
+	digitalWrite(counterResetPin, LOW);
+	counterData = 0b0000000000000000;
+	digitalWrite(valveGatePin, HIGH);
+	Serial.begin(115200);
 }
 
 
+int millisNow;
 void loop() {
-  
+	millisNow = millis();
+	while (millis() - 100 < millisNow) {
+
+	}
+	Serial.println(counterData, BIN);
+	Serial.print('\t');
+	Serial.print(counterData, DEC);
+}
+
+
+void getCount() {
+	counterData << 6;
+	for (int pin = 0; pin <= 9; pin++) {
+		counterData += digitalRead(dataPins[pin]);
+		counterData << 1;
+	}
+	digitalWrite(counterResetPin, HIGH);
+	delay(2);
+	digitalWrite(counterResetPin, LOW);
 }
